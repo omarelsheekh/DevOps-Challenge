2893fa82b3213907450f03cd58df352445bb6575
omarelsheekh <omarelsheekh@DESKTOP-T6O6AL4.localdomain>
2021-02-23 10:58:49 +0200

    helm chart

--- app/app.py
+++ app/app.py
@@ -1,15 +1,16 @@
 from flask import Flask,request,jsonify
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, time
 
 DB_IP=os.getenv('db_ip', '10.0.0.11')
 DB_USERNAME=os.getenv('db_username', 'db_user')
 DB_NAME=os.getenv('db_name', 'halanapp')
 DB_USERPASS=os.getenv('db_userpass', 'db_pass')
+DB_PORT=os.getenv('db_port', 5432)
 
 app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{passwd}@{host_ip}/{db_name}'.format(
-    username=DB_USERNAME, passwd=DB_USERPASS, host_ip=DB_IP, db_name=DB_NAME
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{passwd}@{host_ip}:{db_port}/{db_name}'.format(
+    username=DB_USERNAME, passwd=DB_USERPASS, host_ip=DB_IP, db_name=DB_NAME, db_port=DB_PORT
 )
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
@@ -18,7 +19,16 @@ db=SQLAlchemy(app)
 class IP(db.Model):
     id = db.Column(db.Integer , primary_key=True)
     ip_address = db.Column(db.String, nullable=False)
-db.create_all()
+
+while True:
+    try:
+        db.create_all()
+        break
+    except:
+        print('failed to connect to db \ntrying again in 5s')
+        print('connection string:{}'.format(app.config['SQLALCHEMY_DATABASE_URI']))
+        time.sleep(5)
+
 @app.route('/')
 def index():
     for i in request.args:
